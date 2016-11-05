9928951a36349a586d0572da44c31733bf9e7a44
Salt001 <salt.exe@gmail.com>
2016-11-05 20:20:26 +0000

    Created PrivateVoiceChannel Parser and Container

--- src/me/Salt/Parser/Commands/Admin/PrivateVoiceChannelParser.java
+++ src/me/Salt/Parser/Commands/Admin/PrivateVoiceChannelParser.java
@@ -4,7 +4,13 @@ package me.Salt.Parser.Commands.Admin;
  * Created by Salt001 on 05/11/2016.
  */
 public class PrivateVoiceChannelParser {
-
+    /**
+     * Allows user with the permissions to block other users entering their channel
+     * List of blocked users can also be configured
+     *
+     * By default, all users are blocked. Whitelisted users are saved in file unique to user.
+     *
+     */
     public PrivateVoiceChannelContainer parse(){
 
         return new PrivateVoiceChannelContainer();
