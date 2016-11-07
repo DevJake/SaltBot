635e4def5ecccf06e1df41cfc78a307b02dfc05c
Jake <Salt001@users.noreply.github.com>
2016-11-07 01:36:00 +0000

    Added VoiceChannel muting permissions. Removed *_CROSSGUILD_PERM/TEMP, as individual permissions are managed per-guild. Also added *_CROSSGUILD permission, to represent the allowance of cross-guild commands

--- src/me/Salt/Util/Permissions.java
+++ src/me/Salt/Util/Permissions.java
@@ -1,6 +1,6 @@
 package me.Salt.Util;
 
-public enum Permissions {
+public enum Permissions { //Also used for Restrictions, as permission-states are neutral before being analysed for a prefix in the config file
 //User banning
 BAN_USER_TEMP,
 BAN_USER_PERM,
@@ -8,8 +8,7 @@ BAN_USER_BY_NAME,
 BAN_USER_BY_ID,
 BAN_MULTIPLE_USERS_BY_NAME,
 BAN_MULTIPLE_USERS_BY_ID,
-BAN_USER_CROSSGUILD_PERM,
-BAN_USER_CROSSGUILD_TEMP,
+BAN_USER_CROSSGUILD,
 BAN_USER_BY_ROLE,
 
 //User muting
@@ -19,8 +18,7 @@ MUTE_USER_BY_NAME,
 MUTE_USER_BY_ID,
 MUTE_MULTIPLE_USERS_BY_NAME,
 MUTE_MULTIPLE_USERS_BY_ID,
-MUTE_USER_CROSSGUILD_PERM,
-MUTE_USER_CROSSHUILD_TEMP,
+MUTE_USER_CROSSGUILD,
 MUTE_USER_BY_ROLE,
 
 //VoiceChannel Banning
@@ -30,6 +28,24 @@ BAN_CHANNEL_BY_NAME,
 BAN_CHANNEL_BY_ID,
 BAN_MULTIPLE_CHANNELS_BY_NAME,
 BAN_MULTIPLE_CHANNELS_BY_ID,
-BAN_CHANNEL_CROSSGUILD_PERM,
-BAN_CHANNEL_CROSSGUILD_TEMP
+BAN_CHANNEL_CROSSGUILD,
+
+//VoiceChannel Muting
+MUTE_CHANNEL_TEMP,
+MUTE_CHANNEL_PERM,
+MUTE_CHANNEL_BY_NAME,
+MUTE_CHANNEL_BY_ID,
+MUTE_MULTIPLE_CHANNELS_BY_NAME,
+MUTE_MULTIPLE_CHANNELS_BY_ID,
+MUTE_CHANNEL_CROSSGUILD
+
+//Permission editing
+//User deafening
+//VoiceChannel deafening
+//User config editing
+//Guild config editing
+//System config editing
+//Admin abilities
+//General commands
+//Logging permissions
 }
