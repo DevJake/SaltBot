308e07c183331d5d457b9b3a681bc640f0b3be5d
Salt001 <salt.exe@gmail.com>
2016-11-09 17:59:28 +0000

    Removed unedited files
    Removed files that lack correct code, so the project can successfully
    compile. Only a temporary change

--- src/me/Salt/Parser/Admin/User/ViewLogHistoryContainer.java
+++
@@ -1,7 +0,0 @@
-package me.Salt.Parser.Admin.User;
-
-/**
- * Created by Salt001 on 05/11/2016.
- */
-public class ViewLogHistoryContainer {
-}
--- src/me/Salt/Parser/General/Guild/ViewLogHistoryContainer.java
+++
@@ -1,7 +0,0 @@
-package me.Salt.Parser.General.Guild;
-
-/**
- * Created by Salt001 on 05/11/2016.
- */
-public class ViewLogHistoryContainer {
-}
