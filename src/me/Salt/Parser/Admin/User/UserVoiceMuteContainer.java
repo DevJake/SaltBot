061900392c2fde311804adb666f42a8ee4f2f243
Salt001 <salt.exe@gmail.com>
2016-11-10 23:32:50 +0000

    All parameters of the mute command are now complete

--- src/me/Salt/Parser/Admin/User/UserVoiceMuteContainer.java
+++ src/me/Salt/Parser/Admin/User/UserVoiceMuteContainer.java
@@ -6,59 +6,92 @@ import net.dv8tion.jda.entities.VoiceChannel;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
 import java.util.Date;
+import java.util.List;
 
 public class UserVoiceMuteContainer {
     private User muter;
-    private User[] mutedUser;
+    private List<User> mutedUsers;
     private Guild guild;
     private Date muteTime;
-    private String[] muteReason;
-    private Date[] muteDuration;
+    private List<String> muteReasons;
+    private int muteDuration;
     private MessageReceivedEvent event;
-    private boolean isTempMute;
-    private VoiceChannel[] mutedVoiceChannel;
+    private List<VoiceChannel> mutedVoiceChannel;
 
-    public UserVoiceMuteContainer(User muter, User[] mutedUser, Guild guild, Date muteTime, String[] muteReason, Date[] muteDuration, VoiceChannel[] mutedVoiceChannel, MessageReceivedEvent event) {
+    public UserVoiceMuteContainer(User muter, List<User> mutedUsers, Guild guild, Date muteTime, List<String> muteReasons, int muteDuration, MessageReceivedEvent event, List<VoiceChannel> mutedVoiceChannel) {
         this.muter = muter;
-        this.mutedUser = mutedUser;
+        this.mutedUsers = mutedUsers;
         this.guild = guild;
         this.muteTime = muteTime;
-        this.muteReason = muteReason;
+        this.muteReasons = muteReasons;
         this.muteDuration = muteDuration;
         this.event = event;
         this.mutedVoiceChannel = mutedVoiceChannel;
-    }
-
-    public VoiceChannel[] getMutedVoiceChannel() {
-        return mutedVoiceChannel;
-    }
-
-    public User[] getMutedUser() {
-        return mutedUser;
-    }
-
-
-    public MessageReceivedEvent getEvent() {
-        return event;
     }
 
     public User getMuter() {
         return muter;
     }
 
+    public void setMuter(User muter) {
+        this.muter = muter;
+    }
+
+    public List<User> getMutedUsers() {
+        return mutedUsers;
+    }
+
+    public void setMutedUsers(List<User> mutedUsers) {
+        this.mutedUsers = mutedUsers;
+    }
+
     public Guild getGuild() {
         return guild;
+    }
+
+    public void setGuild(Guild guild) {
+        this.guild = guild;
     }
 
     public Date getMuteTime() {
         return muteTime;
     }
 
-    public String[] getMuteReason() {
-        return muteReason;
+    public void setMuteTime(Date muteTime) {
+        this.muteTime = muteTime;
     }
 
-    public Date[] getMuteDuration() {
+    public List<String> getMuteReasons() {
+        return muteReasons;
+    }
+
+    public void setMuteReasons(List<String> muteReasons) {
+        this.muteReasons = muteReasons;
+    }
+
+    public int getMuteDuration() {
         return muteDuration;
     }
+
+    public void setMuteDuration(int muteDuration) {
+        this.muteDuration = muteDuration;
+    }
+
+    public MessageReceivedEvent getEvent() {
+        return event;
+    }
+
+    public void setEvent(MessageReceivedEvent event) {
+        this.event = event;
+    }
+
+    public List<VoiceChannel> getMutedVoiceChannel() {
+        return mutedVoiceChannel;
+    }
+
+    public void setMutedVoiceChannel(List<VoiceChannel> mutedVoiceChannel) {
+        this.mutedVoiceChannel = mutedVoiceChannel;
+    }
+
+
 }
