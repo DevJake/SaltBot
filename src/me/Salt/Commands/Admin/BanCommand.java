cb6914247a23ff9e0acecc8dfb39db1f1d33f4c5
Salt001 <salt.exe@gmail.com>
2016-11-14 20:07:20 +0000

    Updated to match abstract Command.java

--- src/me/Salt/Commands/Admin/BanCommand.java
+++ src/me/Salt/Commands/Admin/BanCommand.java
@@ -25,6 +25,11 @@ public class BanCommand implements Command {
     }
 
     @Override
+    public void setWaitResult(String input) {
+
+    }
+
+    @Override
     public String help() {
         return null;
     }
