4f9a66e8fbf481d84e4a5489c6e917b184ccd866
Salt001 <salt.exe@gmail.com>
2016-11-14 20:09:46 +0000

    Updated Command#called() parameters

--- src/me/Salt/Util/Command.java
+++ src/me/Salt/Util/Command.java
@@ -1,16 +1,19 @@
 package me.Salt.Util;
 
+import me.Salt.Listeners.EventListener;
 import me.Salt.Parser.Command.CommandParser;
 
 /**
  * Created by zuezy on 06/08/2016.
  */
 public interface Command {
-    boolean called(CommandParser.CommandContainer cmd);
+    boolean called(CommandParser.CommandContainer cmd, EventListener eventListener);
 
     void action(CommandParser.CommandContainer cmd);
 
     void executed(boolean success);
+
+    void setWaitResult(String input); //Allows for users to confirm actions. Additional user input is sent back to the waiting object
 
     String help();
 
