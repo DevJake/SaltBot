98a00e36ec67faf728de478fdb6db44af5925091
Salt001 <salt.exe@gmail.com>
2016-11-15 23:30:46 +0000

    Nested HashMaps are always evil

--- src/me/Salt/Commands/Admin/DescribeCommand.java
+++ src/me/Salt/Commands/Admin/DescribeCommand.java
@@ -5,21 +5,36 @@ import me.Salt.Listeners.EventListener;
 import me.Salt.Parser.Command.CommandParser;
 import me.Salt.Util.Command;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Salt001 on 15/11/2016.
  */
 public class DescribeCommand implements Command {
     private static HashMap<String, CommandDescription> cmdDescs = new HashMap<>();
+    private static HashMap<String, HashMap<String, String>> paramDescs = new HashMap<>();
+
+    private static HashMap<HashMap<String, String>, Boolean> cmdParamMap = new HashMap<>(); //First string is the prefix used, second string is the name and description, boolean is if the parameter is required
+    private static HashMap<String, Boolean> cmdDefinerMap = new HashMap<>();
+
+    private static Map<String, >
+
     @Override
     public boolean preExecution(CommandParser.CommandContainer cmd, EventListener eventListener) {
         return true;
     }
 
+    private HashMap<String, String> temp1(String prefix, String name, String description){
+        HashMap<String, String> temp1 = new HashMap<>();
+        temp1.put(prefix, name + ":" + description);
+        return temp1;
+    }
     @Override
     public void execution(CommandParser.CommandContainer cmd) {
 
+        cmdParamMap.put(temp1("u:", "UID", "Specifies the username or ID of a user"), true);
     }
 
     @Override
