9339acab6b42f6ed18f65ddd1c46c2a09d80f636
Salt001 <salt.exe@gmail.com>
2016-11-15 19:20:59 +0000

    Added CommandDescription container

---
+++ src/me/Salt/Container/CommandDescription.java
@@ -0,0 +1,13 @@
+package me.Salt.Container;
+
+import me.Salt.Parser.Command.CommandParser;
+
+/**
+ * Created by Salt001 on 15/11/2016.
+ */
+public class CommandDescription {
+
+    public CommandDescription(CommandParser.CommandContainer cmd){
+
+    }
+}
