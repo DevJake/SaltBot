df6c94f1986c48d0deb8caf48f8e09db17f63636
Jake <Salt001@users.noreply.github.com>
2016-11-16 10:52:53 +0000

    Update DescribeCommand.java

--- src/me/Salt/Commands/Admin/DescribeCommand.java
+++ src/me/Salt/Commands/Admin/DescribeCommand.java
@@ -13,13 +13,22 @@ import java.util.Map;
  * Created by Salt001 on 15/11/2016.
  */
 public class DescribeCommand implements Command {
-    private static HashMap<String, CommandDescription> cmdDescs = new HashMap<>();
-
-    @Override
-    public boolean preExecution(CommandParser.CommandContainer cmd, EventListener eventListener) {
-        return true;
-    }
-
+    private static HashMap<String, CommandDescription> cmdDescs = new HashMap<>(); //Stores the command, followed by a CommandDescription
+    private static HashMap<String, List<String>> cmdAlias = new HashMap<>(); 
+    
+    /*
+    Stores the command, followed by a List<String> of the aliases. 
+    If cmdDescs doesn't contain the inputted command, cmdAlias can also be checked.
+    If cmdAlias contains the input, the corresponding key is found, 
+    else, the input was an invalid input. 
+    
+    //TODO: The value to find in cmdAlias is used as the 'key'. 
+    May be problematic, but swapping the values is difficult. 
+    May be easier to use a <String, String> HashMap, and store
+    each alias as <Alias, Command>. Will create a potentially larger
+    HashMap, but will resolve any issues. 
+    */
+    
     private HashMap<String, Boolean> createMap(String[] data, String[] name, String[] description, Boolean[] required){
         if (data.length+name.length+description.length+required.length==data.length*4) {
             HashMap<String, Boolean> temp1 = new HashMap<>();
@@ -30,9 +39,10 @@ public class DescribeCommand implements Command {
         }
         return null;
     }
-    @Override
-    public void execution(CommandParser.CommandContainer cmd) {
 
+    @Override
+    public boolean preExecution(CommandParser.CommandContainer cmd, EventListener eventListener) {
+        
         cmdDescs.put("mute", new CommandDescription(
                 createMap(
                         new String[]{"u", "d", "r", "vc"},
@@ -53,13 +63,21 @@ public class DescribeCommand implements Command {
                 false,
                 false));
 
-        cmdDescs.put("deafen", new CommandDescription(
-                null,
-                null,
-                "Allows for the server deafening of a user or users, by name, part name, or ID",
+        cmdDescs.put("stats", new CommandDescription(
+                null, //Change
+                null, //Change
+                "Returns a selection of statistics for SaltBot",
                 "Salt001",
                 false,
                 false));
+        
+        return true;
+    }
+    
+    @Override
+    public void execution(CommandParser.CommandContainer cmd) {
+
+        
     }
 
     @Override
