a001b63ac0ecab6ae3c7b1952f522aa31abe72bb
Salt001 <salt.exe@gmail.com>
2016-11-18 22:32:47 +0000

    Moved both overloaded getDeck() methods

--- src/me/Salt/Handlers/Games/CardsAgainstDiscord/CaDContainer.java
+++ src/me/Salt/Handlers/Games/CardsAgainstDiscord/CaDContainer.java
@@ -1,7 +1,9 @@
 package me.Salt.Handlers.Games.CardsAgainstDiscord;
 
+import me.Salt.Handlers.Games.CardsAgainstDiscord.Util.CardPacks;
 import net.dv8tion.jda.entities.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,10 +12,31 @@ import java.util.List;
 public class CaDContainer {
     private User gameOwner;
     private List<CaHPlayer> players;
-    private List<Cards> cardpacks;
     private int winningScore;
     private int cardsPerPlayer;
     private double roundDelay;
 
+    public CardDeck getDeck(CardPacks cardpack) {
+        switch (cardpack) {
+            case BASE_GAME_1: return new CardDeck(new String[]{"John"}, new String[]{"Me"});
+            case BASE_GAME_1_3: return new CardDeck(new String[]{}, new String[]{});
+            default:
+                return null;
+        }
+    }
+
+    public List<CardDeck> getDeck(List<CardPacks> cardpack) {
+        List<CardDeck> cardDecks = new ArrayList<>();
+        for (CardPacks pack : cardpack) {
+            switch (pack) {
+                case BASE_GAME_1:
+                    cardDecks.add(new CardDeck(new String[]{"John"}, new String[]{"Me"}));
+                default:
+                    return null;
+            }
+        }
+
+        return cardDecks.size() > 0 ? cardDecks : null;
+    }
 
 }
