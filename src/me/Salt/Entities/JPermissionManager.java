bc439bfffe01285e124e21276426a0623f9461ce
Salt001 <salt.exe@gmail.com>
2016-11-19 20:32:19 +0000

    Reorganisation

--- src/me/Salt/Entities/JPermissionManager.java
+++ src/me/Salt/Entities/JPermissionManager.java
@@ -8,10 +8,17 @@ import java.util.List;
  * Created by Salt001 on 19/11/2016.
  */
 public interface JPermissionManager {
-    boolean hasPermission(Permission permission);
 
-    void addPermission();
+    void addPermission(JUser jUser, JGuild jGuild);
 
     List<Permission> getPermissions(JUser jUser);
+
+    List<Permission> getPermissions(JUser jUser, JGuild jGuild);
+
+    boolean hasPermission(Permission permission, JUser jUser, JGuild jGuild);
+
+    void removePermission(JUser jUser, JGuild jGuild);
+
+
 
 }
