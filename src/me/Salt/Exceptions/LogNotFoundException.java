c01495fb37b752f8db2cab40619ba5c33e10b819
Salt001 <salt.exe@gmail.com>
2016-11-19 20:31:11 +0000

    Added LogNotFoundException with message

---
+++ src/me/Salt/Exceptions/LogNotFoundException.java
@@ -0,0 +1,12 @@
+package me.Salt.Exceptions;
+
+/**
+ * Created by Salt001 on 19/11/2016.
+ */
+public class LogNotFoundException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "No logfiles could be found";
+    }
+}
