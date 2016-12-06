ee65e71cee039b3ea08a5fd7f744a3987973db05
Salt001 <salt.exe@gmail.com>
2016-12-06 23:47:32 +0000

    Created CaDParser

---
+++ src/me/Salt/Entities/Games/CardsAgainstDiscord/Container/CaDParser.java
@@ -0,0 +1,14 @@
+package me.Salt.Entities.Games.CardsAgainstDiscord.Container;
+
+import me.Salt.Parser.Command.CommandParser;
+
+/**
+ * Created by Salt001 on 06/12/2016.
+ */
+public class CaDParser {
+
+    public CaDContainer parse(CommandParser.CommandContainer cmd){
+
+        return new CaDContainer();
+    }
+}
